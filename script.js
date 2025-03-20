f9c40133f28d755d966f3394197e6b0d3a2d2471
Cleiton Jeferson <cleclejeje1313@gmail.com>
2025-03-20 19:48:59 -0300

    segundo commit

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function nextImage() {
     }
     document.getElementById('radio' + count).checked = true;
 }
+
+
+// agora sera a parte dos serviços
+
+let currentIndex = 0;
+const itemsPerPage = 3; // Quantos itens você quer mostrar de cada vez
+const ul = document.getElementById('ul-servicos');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+// euj9ru9rew
+
